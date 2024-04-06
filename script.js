a585e36effecb2e74b18859baa542fa57494171c
Fidelwokoma <138219134+Fidelwokoma@users.noreply.github.com>
2024-04-06 14:13:34 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,47 @@
+const paymentForm = document.getElementById('paymentForm');
 
+paymentForm.addEventListener("submit", payWithPaystack, false);
+
+function payWithPaystack(e) {
+
+  e.preventDefault();
+
+
+  let handler = PaystackPop.setup({
+
+    key: 'pk_test_ed7bb70683ea41df964b779492b958f55f984352', // Replace with your public key
+
+    email: document.getElementById("email-address").value,
+
+    plan: 'PLN_f9kanbfpkd6eodd',
+    //quantity: 1,
+    //interval: 'monthly',//
+
+
+    ref: ''+Math.floor((Math.random() * 1000000000) + 1), // generates a pseudo-unique reference. Please replace with a reference you generated. Or remove the line entirely so our API will generate one for you
+
+    // label: "Optional string that replaces customer email"
+
+    onClose: function(){
+
+      alert('Window closed.');
+
+    },
+
+    callback: function(response){
+
+      let message = 'Payment complete! Reference: ' + response.reference;
+
+      alert(message);
+
+    }
+
+  });
+
+
+  handler.openIframe();
+
+} 
 const textss = document.getElementById('price');
 const btn_change = document.getElementById('btn_change');
 const text_tax = document.getElementById('text_tax');
@@ -20,52 +63,3 @@ function changeprice(e) {
       total_price.textContent = '$ 50'
     }
 }
-
-
-
-const paymentForm = document.getElementById('checkoutForm');
-
-paymentForm.addEventListener("submit", payWithPaystack, false);
-
-
-function payWithPaystack(e) {
-
-    e.preventDefault();
-  
-    let handler = PaystackPop.setup({
-  
-      key: 'pk_test_ed7bb70683ea41df964b779492b958f55f984352', // Replace with your public key
-  
-      email: document.getElementById("email").value,
-  
-      plan: 'PLN_f9kanbfpkd6eodd',
-      //quantity: 1,
-      //interval: 'monthly',//
-  
-      amount: document.getElementById("amount").value * 100,
-  
-      ref: ''+Math.floor((Math.random() * 1000000000) + 1), // generates a pseudo-unique reference. Please replace with a reference you generated. Or remove the line entirely so our API will generate one for you
-  
-      // label: "Optional string that replaces customer email"
-  
-      onClose: function(){
-  
-        alert('Window closed.');
-  
-      },
-  
-      callback: function(response){
-  
-        let message = 'Payment complete! Reference: ' + response.reference;
-  
-        alert(message);
-  
-      }
-  
-    });
-  
-  
-    handler.openIframe();
-  
-  }
-  
